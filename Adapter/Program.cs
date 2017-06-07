7627c852e5562b3d5e47761d7ba2563c52737c6a
aniefefalas <aniefefalas@outlook.com>
2017-06-07 15:20:04 +0800

    update namespace

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -3,7 +3,7 @@ using System.IO;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
 
-namespace ConsoleApp3
+namespace Adapter
 {
     class Program
     {
--- Composite/ImageFile.cs
+++ Composite/ImageFile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ConsoleApp4
+namespace Composite
 {
     class ImageFile : AbstractFile
     {
